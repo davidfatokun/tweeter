ca8ba8ef53fbb4c61e74ea384c87321b71adc349
David <fatokundavid2@gmail.com>
2023-06-24 13:55:42 -0600

    complete displaying validation errors with jquery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,11 +1,14 @@
-// const { format } = require('timeago.js');
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
 $(document).ready(function () {
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
   const createTweetElement = function (tweet) {
     const userName = tweet.user.name;
@@ -14,17 +17,17 @@ $(document).ready(function () {
     const text = tweet.content.text;
     const timestamp = timeago.format(tweet.created_at);
 
-    let $tweet = $(`
+  let $tweet = $(`
   <article class="tweet">
     <header id="tweet-superscript">
-      <span> <img class="avatar" src="${userAvatar}" width="100" height="100" />${userName}</span>
-      <h3>${handle}</h3>
+      <span> <img class="avatar" src="${escape(userAvatar)}" width="100" height="100" />${escape(userName)}</span>
+      <h3>${escape(handle)}</h3>
     </header>
-    <p>${text}</p>
+    <p>${escape(text)}</p>
     <hr>
     </hr>
     <footer id="icons">
-      <span>${timestamp}</span>
+      <span>${escape(timestamp)}</span>
       <div>
         <a href="#" class="icon"><i name="flag" class="fas fa-flag"></i></a>
         <a href="#" class="icon"><i class="fas fa-heart"></i></a>
@@ -59,11 +62,18 @@ $(document).ready(function () {
   const onSubmit = $("form").on("submit", function (event) {
     event.preventDefault();
     if ($('#tweet-text').val().length === 0) {
-      alert("Tweet cannot be empty");
-    }
-    if ($('#tweet-text').val().length > 140) {
-      alert("Tweet is over 140 characters");
+      // alert("Tweet cannot be empty");
+      $(".error").html("<i class='fa-solid fa-triangle-exclamation'></i> Tweet cannot be empty <i class='fa-solid fa-triangle-exclamation'></i>");
+      $(".error").slideDown();
+      $(".error").css('display', 'flex');
+    }else if ($('#tweet-text').val().length > 140) {
+      // alert("Tweet is over 140 characters");
+      $(".error").html("<i class='fa-solid fa-triangle-exclamation'></i> Too long. Please respect our arbitrary limit of 140 characters <i class='fa-solid fa-triangle-exclamation'></i>");
+      $(".error").slideDown();
+      $(".error").css('display', 'flex');
     } else {
+      $(".error").slideUp();
+      $(".error").css('display', 'none');
       $.ajax({
         url: "/tweets",
         method: "POST",
